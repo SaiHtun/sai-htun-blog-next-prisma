84fd3d1e2c6ee9cfc23ada9105c033a122b115bf
saihtun <saihtun1430@gmail.com>
2021-04-09 11:08:11 -0700

    :rocket:  hello nextjs and prisma

---
+++ lib/prisma.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!globalThis.prisma) {
+    globalThis.prisma = new PrismaClient();
+  }
+  prisma = globalThis.prisma;
+}
+
+export default prisma;
